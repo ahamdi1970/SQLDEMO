eb13a4ed53ef9b6acf2f1bbdb152e2d1d9a20f36
ahamdi1970 <86688764+ahamdi1970@users.noreply.github.com>
2021-11-12 14:51:27 +0100

    try catch creating customer to use exception

--- app/src/main/java/com/openclassrooms/sqldemo/MainActivity.java
+++ app/src/main/java/com/openclassrooms/sqldemo/MainActivity.java
@@ -1,20 +1,26 @@
 package com.openclassrooms.sqldemo;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.jar.JarEntry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     // references to buttons and other controls in the layout
+
     Button btn_add, btn_viewAll;
+
     EditText et_name, et_age;
     Switch sw_activeCustomer;
-    ListView lv_customer_List;
+    ListView lv_customerList;
 
 
     @Override
@@ -27,23 +33,31 @@ public class MainActivity extends AppCompatActivity {
         et_age = findViewById ( R.id.et_age );
         et_name = findViewById ( R.id.et_name );
         sw_activeCustomer = findViewById ( R.id.sw_active );
-        lv_customer_List = findViewById ( R.id.lv_customerList );
+        lv_customerList = findViewById ( R.id.lv_customerList );
 
         // button listeners for the add and view all buttons
 
-//        btn_add.setOnClickListener ( new View.OnClickListener () {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText ( MainActivity.this,"add", Toast.LENGTH_SHORT ).show ();
-//            }
-//        } );
-//
-//        btn_viewAll.setOnClickListener ( new View.OnClickListener () {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText ( MainActivity.this,"View all", Toast.LENGTH_SHORT ).show ();
-//            }
-//        } );
+        btn_add.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                try {
+                    CustomerModel customerModel = new CustomerModel ( -1,et_name.getText ().toString (),Integer.parseInt (et_age.getText ().toString ()),sw_activeCustomer.isChecked () );
+                    Toast.makeText ( MainActivity.this,customerModel.toString (), Toast.LENGTH_SHORT ).show ();
+                }
+                catch (Exception e){
+                    Toast.makeText ( MainActivity.this,"Error creating customer", Toast.LENGTH_SHORT ).show ();
+                }
+
+
+            }
+        } );
+
+        btn_viewAll.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText ( MainActivity.this,"View all", Toast.LENGTH_SHORT ).show ();
+            }
+        } );
 
     }
 }
